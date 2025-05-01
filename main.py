a9ea445aac4f6ae7cc1dfc2560efc063b34fd8d6
RWalker92 <ryan.walker1992@gmail.com>
2025-05-01 15:37:38 +0100

    added cards library and basic welcome text

---
+++ main.py
@@ -0,0 +1,18 @@
+import random
+cards = {"A": [1, 11], "2": [2], "3": [3], "4": [4], "5": [5], "6": [6], "7": [7], "8": [8], "9": [9], "10": [10], "J": [10], "Q": [10], "K": [10]}
+
+player_hand = []
+dealer_hand = []
+player_score = 0
+dealer_score = 0
+
+player_name = input("Enter your name: ")
+print(f"Welcome to Blackjack, {player_name}!")
+print("The goal of the game is to get as close to 21 as possible without going over.")
+print("You will be playing against the dealer. Let's start!")
+print("Dealing cards...")
+
+# Deal two cards to the player and one card to the dealer
+
+
+
